751edc62ced1959c9498531e7c68482bd8773989
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2024-08-18 19:31:42 +0800

    fix: do not drop profile activity

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
@@ -20226,13 +20226,13 @@ public class MessagesController extends BaseController implements NotificationCe
             reason = getRestrictionReason(chat.restriction_reason);
         } else {
             reason = getRestrictionReason(user.restriction_reason);
-            if (type != 3 && user.bot) {
-                type = 1;
-                BaseFragment lastFragment = LaunchActivity.getLastFragment();
-                if (lastFragment.getLastStoryViewer() == null) {
-                    closeLast = true;
-                }
-            }
+//            if (type != 3 && user.bot) {
+//                type = 1;
+//                BaseFragment lastFragment = LaunchActivity.getLastFragment();
+//                if (lastFragment.getLastStoryViewer() == null) {
+//                    closeLast = true;
+//                }
+//            }
         }
         boolean doNotCloseLast = false;
         if (LaunchActivity.getLastFragment() instanceof DialogsActivity) {
